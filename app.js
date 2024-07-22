b7dd4cd79d7b861a56278c212cb3c7cd0253406a
yeonsang <wjddustkd45@gmail.com>
2024-07-22 22:13:03 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config();
+
+var indexRouter = require('./app/routes/index');
+var usersRouter = require('./app/routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
+module.exports = app;
